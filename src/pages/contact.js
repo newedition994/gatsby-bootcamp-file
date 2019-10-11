5e3b915e8d20102294a6d56e4a9e4a84c67ffa0d
Breyanna Vass <breyannavass@gmail.com>
2019-10-11 17:33:43 -0400

    links open new tabs

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,10 @@ const ContactPage = () => {
       <h1>Contact Me</h1>
       <p>
         Please view my{" "}
-        <a href="https://www.linkedin.com/in/breyanna-vass-6bb97773/">
+        <a
+          href="https://www.linkedin.com/in/breyanna-vass-6bb97773/"
+          target="_blank"
+        >
           LinkedIn
         </a>{" "}
         profile for more about my experiences.
