30a432453c0f6223a48fd15cd64afa1b4a6a27e5
Breyanna Vass <breyannavass@gmail.com>
2019-10-11 17:19:08 -0400

    adding an about and contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>Different ways to contact me.</p>
+    </div>
+  )
+}
+
+export default ContactPage
