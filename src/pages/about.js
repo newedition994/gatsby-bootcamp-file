2443f2bf1ae3c519e813c3ab8b190ec1bf095367
Breyanna Vass <breyannavass@gmail.com>
2019-10-11 18:14:47 -0400

    adding a layout for all the pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Footer from "../components/footer"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 const AboutPage = () => {
   return (
     <div>
-      <Header />
-      <h1>About</h1>
-      <p>
-        Loved what you react please <Link to="/contact">contact me</Link>.
-      </p>
-      <Footer />
+      <Layout>
+        <h1>About</h1>
+        <p>
+          Loved what you react please <Link to="/contact">contact me</Link>.
+        </p>
+      </Layout>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,24 +1,23 @@
 import React from "react"
 
-import Footer from "../components/footer"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 const ContactPage = () => {
   return (
     <div>
-      <Header />
-      <h1>Contact Me</h1>
-      <p>
-        Please view my{" "}
-        <a
-          href="https://www.linkedin.com/in/breyanna-vass-6bb97773/"
-          target="_blank"
-        >
-          LinkedIn
-        </a>{" "}
-        profile for more about my experiences.
-      </p>
-      <Footer />
+      <Layout>
+        <h1>Contact Me</h1>
+        <p>
+          Please view my{" "}
+          <a
+            href="https://www.linkedin.com/in/breyanna-vass-6bb97773/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>{" "}
+          profile for more about my experiences.
+        </p>
+      </Layout>
     </div>
   )
 }
