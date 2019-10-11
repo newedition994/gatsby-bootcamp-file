974b3840756c75e1065841c5cf8b92bc190e8c0d
Breyanna Vass <breyannavass@gmail.com>
2019-10-11 18:40:21 -0400

    making self contained styles for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import headerStyles from "./header.module.scss"
+
 const Header = () => {
   return (
     <header>
@@ -8,7 +10,9 @@ const Header = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link className={headerStyles.link} to="/">
+              Home
+            </Link>
           </li>
           <li>
             <Link to="/about">About</Link>
