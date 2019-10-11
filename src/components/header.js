089bea2427e32b8cc80e501dbb3a0358a81a6824
Breyanna Vass <breyannavass@gmail.com>
2019-10-11 17:50:24 -0400

    adding links to pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Header = () => {
   return (
     <header>
       <p>Breyanna the Dev</p>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
     </header>
   )
 }
