fefd4d0e73d59a7d57481f78424c68b3ab7e47f5
Breyanna Vass <breyannavass@gmail.com>
2019-10-11 18:05:23 -0400

    adjusting the nav links

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,20 @@ const Header = () => {
     <header>
       <p>Breyanna the Dev</p>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/contact">Contact</Link>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   )
