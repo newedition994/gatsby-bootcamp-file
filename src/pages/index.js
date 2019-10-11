602363fa373b453e3ae5a0f8538f8c0f5ba60137
Breyanna Vass <breyannavass@gmail.com>
2019-10-11 17:26:51 -0400

    adding links on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello</h1>
       <h2>I am Breyanna Vass</h2>
+      <p>
+        Need a developer? <a href="/contact">Contact Me</a>
+      </p>
+      <p>
+        Need a developer? <Link to="/contact">Contact me</Link>
+      </p>
     </div>
   )
 }
